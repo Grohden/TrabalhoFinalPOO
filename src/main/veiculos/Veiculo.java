69075297151e432ec78d61f732c129799a99147c
Gabriel Rohden <gabriel_rohden@outlook.com>
2017-06-10 13:10:15 -0300

    Adicionado javadoc a classe base veiculo

--- src/main/veiculos/Veiculo.java
+++ src/main/veiculos/Veiculo.java
@@ -2,6 +2,12 @@ package main.veiculos;
 
 import java.util.Calendar;
 
+/**
+ * @author Gabriel Rohden
+ * @version 0.2
+ *          Classe base para veiculos
+ * @since 10/06/2017
+ */
 public abstract class Veiculo {
     private Calendar anoFabricacao;
     private String numeroPlaca;
@@ -10,61 +16,124 @@ public abstract class Veiculo {
     private Marca marca;
     private ModeloVeiculo modelo;
 
-    public Veiculo(){
+    /**
+     * Instancia da classe base para veiculos
+     */
+    public Veiculo() {
 
     }
 
+    /**
+     * Seta o modelo do veiculo
+     *
+     * @param modelo modelo do veiculo
+     * @return a instancia do objeto para 'chain sets'
+     * @see main.veiculos.ModeloVeiculo
+     */
     public Veiculo setModelo(ModeloVeiculo modelo) {
         this.modelo = modelo;
         return this;
     }
 
+    /**
+     * Seta a quantidade de quilometros rodados pelo veiculo
+     *
+     * @param quilometragem quilometragem rodada.
+     * @return a instancia do objeto para 'chain sets'
+     */
     public Veiculo setQuilometragem(long quilometragem) {
         this.quilometragem = quilometragem;
         return this;
     }
 
+    /**
+     * Seta o valor do veiculo
+     *
+     * @param valorDoVeiculo valor em reais do veiculo
+     * @return a instancia do objeto para 'chain sets'
+     */
     public Veiculo setValorDoVeiculo(long valorDoVeiculo) {
         this.valorDoVeiculo = valorDoVeiculo;
         return this;
     }
 
+    /**
+     * Seta o ano de fabricacao do veiculo
+     *
+     * @param anoFabricacao instancia do calendar com o ano de fabricacao
+     * @return a instancia do objeto para 'chain sets'
+     */
     public Veiculo setAnoFabricacao(Calendar anoFabricacao) {
         this.anoFabricacao = anoFabricacao;
         return this;
     }
 
+    /**
+     * Seta o numero da placa do veiculo, para posterior identificacao
+     *
+     * @param numeroPlaca numero da placa do veiculo
+     * @return a instancia do objeto para 'chain sets'
+     */
     public Veiculo setNumeroPlaca(String numeroPlaca) {
         this.numeroPlaca = numeroPlaca;
         return this;
     }
 
+    /**
+     * Seta a marca do veiculo
+     *
+     * @param marca Marca do veiculo
+     * @return a instancia do objeto para 'chain sets'
+     * @see main.veiculos.Marca
+     */
     public Veiculo setMarca(Marca marca) {
         this.marca = marca;
         return this;
     }
 
-    public Calendar getAnoFabricacao() {
-        return anoFabricacao;
-    }
-
-    public String getNumeroPlaca() {
-        return numeroPlaca;
-    }
-
-    public Marca getMarca() {
-        return marca;
-    }
-
+    /**
+     * @return o modelo do veiculo
+     * @see main.veiculos.ModeloVeiculo
+     */
     public ModeloVeiculo getModelo() {
         return modelo;
     }
 
+    /**
+     * @return o ano de fabricacao do veiculo
+     */
+    public Calendar getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    /**
+     * @return o nomero da placa registrado
+     */
+    public String getNumeroPlaca() {
+        return numeroPlaca;
+    }
+
+    /**
+     * @return a marca do modelo
+     * @see main.veiculos.Marca
+     */
+    public Marca getMarca() {
+        return marca;
+    }
+
+    /**
+     * @return a quilometragem rodada pelo veiculo
+     */
     public long getQuilometragem() {
         return quilometragem;
     }
 
+    /**
+     * @return O valor do veiculo
+     */
     public long getValorDoVeiculo() {
         return valorDoVeiculo;
     }
+
+
 }
